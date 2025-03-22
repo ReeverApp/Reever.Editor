777506c6466c5a2cefc07e3a6569dad198d7b3f9
Tansu Aşıcı <gamex75369@gmail.com>
2025-03-22 23:06:20 +0300

    feat: add EditorComponent with rich text editing capabilities and various inline tools

---
+++ reever-editor/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditorComponent from './components/EditorComponent';
+
+function App() {
+  return (
+      <div className="App">
+        <EditorComponent />
+      </div>
+  );
+}
+
+export default App;
---
+++ reever-editor/src/types/types.d.ts
@@ -0,0 +1,5 @@
+declare module "@editorjs/header"
+declare module "@editorjs/checklist"
+declare module "@calumk/editorjs-nested-checklist"
+declare module "editorjs-drag-drop"
+declare module 'editorjs-strikethrough'
